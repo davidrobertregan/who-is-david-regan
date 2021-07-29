3d909eae20a602fbfff87a03db2d7ad71d7c12a8
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-07-29 13:15:25 -0600

    javascript button images

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-/* const p = document.createElement("p");
-p.textContent = "always ready to learn";
-document.querySelector('body').appendChild(p);
+        const up = document.getElementById('up');  
+          
+        function addBanjo() {
+            const banjo = document.createElement('img');
+            banjo.src = "Banjo.JPG";
+            banjo.style.width = "400px";
+            document.getElementById('buttons').appendChild(banjo);
+        } 
 
-in HTML: <script src = "script.js"></script>
+        function addUltra() {
+            const rocky = document.createElement('img');
+            rocky.src = "Rocky-50.JPG";
+            rocky.style.width = "400px";
+            document.getElementById('buttons').appendChild(rocky);
+        }
 
-*/
+        function addMountains() {
+            const middle = document.createElement('img');
+            middle.src ="middle.jpeg";
+           // How can i add a link to image?...  middle.setAttribute("href", "https://www.strava.com/activities/5676545097");
+            middle.style.width="400px";
+
+            document.getElementById('buttons').appendChild(middle);
+        }
